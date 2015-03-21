ee8e0ef100a83b80bdcdc17c3aecb4126886a569
凹凸曼 <543484525@qq.com>
2015-03-21 18:41:06 +0800

    Modify product page file

--- js/common.js
+++ js/common.js
@@ -1,30 +1,44 @@
-/**
- * Created by Administrator on 2015/1/13 0013.
- */
-$(function(){
-    var font;
-    var win_W = $(window).width();
 
-    if(win_W>=364 && win_W<=375){
-        font=24;
-    }else{
+    /**
+     * Created by Administrator on 2015/1/13 0013.
+     */
+    (function(){
 
-        if(win_W==360 || win_W==361){
+        /* 添加用户窗口事件 改变根字号大小 */
+        function addEvent(obj,type,fn){
+            if(obj.addEventListener){
+                obj.addEventListener(type,fn,false);
+            } else {
+                obj.attachEvent('on'+type,fn);
+            }
+        }
 
-            font=22;
 
-        }else{
-
-            if(win_W>540){
-                font=34;
+        function autoSize(){
+            var font = Math.round(win_W / 16);
+            var HTML=document.getElementById('html');
+            var win_W = $(".container").width();
+            if(win_W>=364 && win_W<=375){
+                font=24;
             }else{
-                font = Math.round(win_W / 16);
+
+                if( win_W == 360 || win_W == 361){
+                    font=22;
+                }else{
+                    if( win_W > 540 ){
+                        font=34;
+                    }else{ font = Math.round( win_W / 16 ); }
+                }
             }
 
-        }
-    }
-    var HTML=document.getElementById('html');
-    HTML.style.fontSize = font + 'px';
+            HTML.style.fontSize = font + 'px';
 
-})
+
+        }
+        addEvent(window,'load',autoSize);
+        addEvent(window,'resize',autoSize);
+        /*  end 添加用户窗口事件 */
+
+
+    })()
 
